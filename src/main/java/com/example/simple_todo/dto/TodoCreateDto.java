0c33b767c226d87aa378c552bfbfed4fee92f411
Student <Student@DESKTOP-TBJEKOB>
2022-01-14 22:28:31 +0300

    Fixed bug that prevent user to create new todo

--- src/main/java/com/example/simple_todo/dto/TodoCreateDto.java
+++ src/main/java/com/example/simple_todo/dto/TodoCreateDto.java
@@ -1,11 +1,13 @@
 package com.example.simple_todo.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class TodoCreateDto {
     @NotBlank(message = "Title is mandatory")
