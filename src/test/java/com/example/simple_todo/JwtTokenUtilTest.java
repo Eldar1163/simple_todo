a05ea4861753efdf7f4dbb7d400293999b02f8b4
Student <gusejnov_eldar@mail.ru>
2022-01-28 18:29:04 +0300

    Added new test, rewrote old

--- src/test/java/com/example/simple_todo/JwtTokenUtilTest.java
+++ src/test/java/com/example/simple_todo/JwtTokenUtilTest.java
@@ -2,62 +2,93 @@ package com.example.simple_todo;
 
 import com.example.simple_todo.config.ConfigProperties;
 import com.example.simple_todo.domain.User;
+import com.example.simple_todo.dto.UserClaims;
 import com.example.simple_todo.jwt_util.JwtTokenUtil;
 import com.example.simple_todo.service.UserService;
 import io.jsonwebtoken.Jwts;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Date;
 
-import static org.junit.jupiter.api.Assertions.fail;
-import static org.mockito.Mockito.mock;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 public class JwtTokenUtilTest {
 
-    private final UserService userService = mock(UserService.class);
+    @Mock
+    private UserService userService;
 
-    private final JwtTokenUtil jwtTokenUtil;
+    private JwtTokenUtil jwtTokenUtil;
 
-    private final ConfigProperties configProperties;
+    private static final String secret = "dsVkjsfcejkASFnbAFJKWvlqjndsv";
+
+    private final User user;
+    private final Long userId;
+    private final String username;
 
     public JwtTokenUtilTest() {
+        userId = 7L;
+        username = "user";
+
+        user = new User();
+        user.setId(userId);
+        user.setUsername(username);
+    }
+
+    @BeforeEach
+    void init() {
         ConfigProperties configProperties = new ConfigProperties();
         configProperties.setJwt(new ConfigProperties.Jwt());
-        configProperties.getJwt().setSecret("dsVkjsfcejkASFnbAFJKWvlqjndsv");
-        this.configProperties = configProperties;
-        this.jwtTokenUtil = new JwtTokenUtil(userService, configProperties);
+        configProperties.getJwt().setSecret(secret);
+        jwtTokenUtil = new JwtTokenUtil(userService, configProperties);
     }
 
     @Test
     public void createTokenTest() {
-        String secret = configProperties.getJwt().getSecret();
-
-        Long userId = 7L;
-        String username = "user";
-
-        User user = new User();
-        user.setId(userId);
-        user.setUsername(username);
-
         when(userService.getUserById(userId)).thenReturn(user);
+        String token = jwtTokenUtil.generateToken(userId);
 
-        String beginning = "eyJhbGciOiJIUzUxMiJ9.eyJ1c2VyLWluZm8iOnsiaWQiOjcsInVzZXJuYW1lIjoidXNlci";
+        UserClaims userClaims = jwtTokenUtil.getUserClaimsFromToken(token);
 
-        String actual = jwtTokenUtil.generateToken(userId);
+        assertEquals(userClaims.getId(), userId);
+        assertEquals(userClaims.getUsername(), username);
+    }
+
+    @Test
+    public void validToken() {
+        when(userService.getUserById(userId)).thenReturn(user);
+        String token = jwtTokenUtil.generateToken(userId);
 
         Date curDate = new Date(System.currentTimeMillis());
-
-        Date expDate = Jwts.parser().setSigningKey(secret).parseClaimsJws(actual).getBody().getExpiration();
+        Date expDate = Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody().getExpiration();
 
         long dateDiff = expDate.getTime() - curDate.getTime();
 
-        boolean correctExpDate = (Math.abs(dateDiff - JwtTokenUtil.JWT_TOKEN_VALIDITY) < 2 * 60 * 1000);
+        boolean correctExpDate = (Math.abs(dateDiff - JwtTokenUtil.JWT_TOKEN_VALIDITY) < 5 * 1000);
+        boolean isSigned = false;
 
-        if (!actual.startsWith(beginning) ||
-                !Jwts.parser().setSigningKey(secret).isSigned(actual) ||
-                !correctExpDate) {
-            fail();
+        try {
+            Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody().getExpiration();
+            isSigned = true;
+        } catch (Exception ignored) {
         }
+
+        assertTrue(correctExpDate);
+        assertTrue(isSigned);
+    }
+
+    @Test
+    public void invalidToken() {
+        when(userService.getUserById(userId)).thenReturn(user);
+        String token = jwtTokenUtil.generateToken(userId);
+        String invalidToken = token.substring(0, token.length() - 1);
+
+        assertTrue(jwtTokenUtil.isValidToken(token));
+        assertFalse(jwtTokenUtil.isValidToken(invalidToken));
     }
 }
